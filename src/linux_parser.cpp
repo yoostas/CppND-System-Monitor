7dcc636cc07272d342af08786c9debfd643d4d3c
Stanislav's MacBook Pro <stanislav.yusupov1989@gmail.com>
2020-06-24 01:26:46 +0200

    kernel version displayed

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -38,12 +38,12 @@ string LinuxParser::OperatingSystem() {
 // DONE: An example of how to read data from the filesystem
 string LinuxParser::Kernel() {
   string os, kernel;
-  string line;
+  string line, version;
   std::ifstream stream(kProcDirectory + kVersionFilename);
   if (stream.is_open()) {
     std::getline(stream, line);
     std::istringstream linestream(line);
-    linestream >> os >> kernel;
+    linestream >> os >> version >> kernel;
   }
   return kernel;
 }
